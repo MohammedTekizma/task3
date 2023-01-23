9bbb23d7e9a675085735dac14005f0c0e0d8a8bc
MohammedTekizma <mohammed.ismail@tekizma.com>
2023-01-23 10:04:56 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { AppServices } from './app.services';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private appServices: AppServices){}
   title = 'task3';
+  httpdata:any;
+  value!: any ;
+  
+  getVal(val: any){
+    this.value = val;
+    if(this.value != '')
+      this.appServices.fetchDataSer(Number(this.value)).subscribe((data: object)=>{
+        this.httpdata = data;})
+    this.appServices.fetchDataSer().subscribe((data: object)=>{
+      this.httpdata = data;})
+  }
+  
+  /* ngOnInit(){
+    this.appServices.fetchDataSer(Number()).subscribe((data: object)=>{
+      this.httpdata = data;})
+      // console.log(this.httpdata);
+    
+  } */
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { AppServices } from './app.services';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AppServices],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
